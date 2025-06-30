a8c9d20f5d6102da8ddac54beee272adef3d82ed
book诗意 <tech.gosling0d@icloud.com>
2025-06-30 09:31:13 +0800

    update

--- worker.js
+++ worker.js
@@ -1,13 +1,6 @@
-import { getAssetFromKV } from "@cloudflare/kv-asset-handler";
-
-addEventListener("fetch", (event) => {
-  event.respondWith(handleEvent(event));
-});
-
-async function handleEvent(event) {
-  try {
-    return await getAssetFromKV(event);
-  } catch (e) {
-    return new Response("Not found", { status: 404 });
-  }
-}
+export default {
+  async fetch(request, env, ctx) {
+    // 让 Cloudflare 自动处理静态资源
+    return env.ASSETS.fetch(request);
+  },
+};
